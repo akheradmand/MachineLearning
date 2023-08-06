06b52ad3c3dfcc18fc91a68fc4d0c7d6ac81b90e
akheradmand <akheradmand2002@yahoo.com>
2023-08-06 20:20:03 +0330

    46-1 update

--- 46/46-1/employee_salary.py
+++ 46/46-1/employee_salary.py
@@ -1,11 +1,9 @@
-import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
 from sklearn.datasets import make_regression
 from sklearn.model_selection import train_test_split
 from perceptron import Perceptron
 
-X,Y,coef = make_regression(n_samples=100, n_features=1, n_informative=1, noise=10, coef=True, random_state=1)
+X,Y,coef = make_regression(n_samples=50, n_features=1, n_informative=1, noise=10, coef=True, random_state=1)
 
 Y=Y.reshape(-1,1)
 
@@ -21,13 +19,18 @@ X_train,X_test,Y_train,Y_test=train_test_split(X,Y,test_size=0.2)
 W=np.random.rand(1,1)
 b=np.random.rand(1,1)
 
-lr_w=0.0001
+lr_W=0.0001
 lr_b=0.1
 
-epochs=20
+epochs=50
 
-perceptron=Perceptron(W,b,lr_w,lr_b,epochs)
+perceptron=Perceptron(W,b,lr_W,lr_b,epochs)
 perceptron.fit(X_train,Y_train)
 
 #predict with X_test
-perceptron.predict(X_test)
+print(perceptron.predict(X_test))
+print(Y_test)
+
+#evaluate
+MSE=perceptron.evaluate(X_test,Y_test)
+print(MSE)
--- 46/46-1/perceptron.py
+++ 46/46-1/perceptron.py
@@ -16,14 +16,15 @@ class Perceptron:
 
         losses=[]
         fig,(ax1,ax2)=plt.subplots(1,2)
+        fig.set_figwidth(10)
 
         for n in range(self.epochs):
             for i in range(self.X_train.shape[0]):
                 x=self.X_train[i]
                 y=self.Y_train[i]
-                y_pred=x*self.W + self.b
+                y_pred_train=x*self.W + self.b
                 
-                error=y-y_pred
+                error=y-y_pred_train
 
                 self.W = self.W + error*x*self.learning_rate_w
                 self.b = self.b + error*self.learning_rate_b
@@ -31,28 +32,37 @@ class Perceptron:
                 loss=np.mean(np.abs(error))
                 losses.append(loss)
 
-                Y_pred=self.X_train*self.W+self.b
+                Y_pred_train=self.X_train*self.W+self.b
 
                 ax1.clear()
                 ax1.scatter(self.X_train,self.Y_train,color="blue")
-                ax1.plot(self.X_train,Y_pred, color="red")
+                ax1.plot(self.X_train,Y_pred_train, color="red")
+                ax1.set_title("perceptron method")
+                ax1.set_xlabel("years of experience")
+                ax2.set_ylabel("salary($)")
 
                 ax2.clear()
                 ax2.plot(losses)
+                ax2.set_title("Loss")
                 plt.pause(0.01)
-        # plt.pause(5)
-        # return ax1
-        fig.clear()
+
+        plt.close()
+        return self.W,self.b
 
     def predict(self,X_test):
+        Y_pred=X_test*self.W + self.b
         
+        return Y_pred
+
+    def evaluate(self,X_test,Y_test):
+        Y_pred=self.predict(X_test)
+        MSE=np.mean(np.square(Y_test-Y_pred))
         plt.scatter(self.X_train,self.Y_train,color="blue")
         plt.plot(self.X_train,self.X_train*self.W+self.b, color="red")
-        Y_pred=X_test*self.W + self.b
-        plt.plot(X_test,Y_pred,color="hotpink")
+        plt.scatter(X_test,Y_test,color="hotpink")
+        plt.legend(['train data','training line','test data'])
+        plt.xlabel("years of experience")
+        plt.ylabel("salary($)")
         plt.show()
-
-    def evaluate(self):
-        ...
-
+        return MSE
     
